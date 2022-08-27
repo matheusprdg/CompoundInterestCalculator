42d65d939e9a69821b70e3763278e95547931120
Matheus <perdigaomatheus@hotmail.com>
2022-08-26 21:06:10 -0300

    Correção de bug e adição de política CORS.

--- src/Domain/CompoundInterestCalculator.Domain.Core/Calculators/IIncomeTaxCalculator.cs
+++ src/Domain/CompoundInterestCalculator.Domain.Core/Calculators/IIncomeTaxCalculator.cs
@@ -1,8 +1,10 @@
+using CompoundInterestCalculator.Domain.Models;
 using CompoundInterestCalculator.Domain.Models.IncomeTaxCalculator;
 
 namespace CompoundInterestCalculator.Domain.Calculators;
 
 public interface IIncomeTaxCalculator
 {
+    bool Compatible(IncomeTaxPeriod incomeTaxPeriod);
     IIncomeTaxCalculatorOutput Calculate(IIncomeTaxCalculatorInput input, CancellationToken cancellationToken);
 }
