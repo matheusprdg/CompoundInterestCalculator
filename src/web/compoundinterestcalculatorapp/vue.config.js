c04563f6f206253c4d25f914f6f30507ea0f3d92
Matheus <perdigaomatheus@hotmail.com>
2022-09-01 20:38:45 -0300

    Correções e implementações.

--- src/web/compoundinterestcalculatorapp/vue.config.js
+++ src/web/compoundinterestcalculatorapp/vue.config.js
@@ -2,5 +2,6 @@ const { defineConfig } = require('@vue/cli-service');
 const { resolve } = require('path');
 
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
 })
